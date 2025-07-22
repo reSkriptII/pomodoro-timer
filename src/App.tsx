e7e72bafc746bd3138a7ac28db6025ecf26fcba6
Cyron <reskriptii@gmail.com>
2025-07-22 13:57:50 +0700

    add timer logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const [timer, setTimer] = useState(1500); // time in second
+  const timerDisplay = Math.floor(timer / 60) + ':' + ('0' + (timer % 60)).slice(-2) ;
+
+  function handleClick() {
+    const intervalId = setInterval(() => {
+      if (timer > 0) {
+        setTimer(t => t - 1);
+      } else {
+        clearInterval(intervalId);
+      }
+      
+    }, 1000);
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div> {timerDisplay} </div>
+      <button onClick={handleClick} />
     </>
+    
   )
 }
 
-export default App

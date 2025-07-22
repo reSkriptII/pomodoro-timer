64186223f01d1222dfd42432c950b74cc7d28db4
Cyron <reskriptii@gmail.com>
2025-07-23 00:00:58 +0700

    add styling

--- src/Timer.tsx
+++ src/Timer.tsx
@@ -15,23 +15,38 @@ export function Timer({ stage }: { stage: stage_t }) {
       timerIntervalRef.current = setInterval(() => {
         setTimer(t => {
           const nextTimer = t - 1;
+
           if (nextTimer <= 0) {
             clearInterval(timerIntervalRef.current)
             setIsRunning(false);
+
+            notify(stage)
+
+            return 0;
           }
+
           return nextTimer;
+
         })}, 1000);
     } else {
       clearInterval(timerIntervalRef.current)
+      
     }
   }
 
   return (
     <>
       <TimerDisplay timer={timer} />
-      <button onClick={handleClick}>
-        {isRunning? 'pause' : 'start'}
-      </button>
+      <div>
+        <button 
+          onClick={handleClick} 
+          data-active={isRunning}
+          className="block text-2xl w-36 shadow-lg shadow-amber-700 py-2 bg-white mx-auto rounded-md data-[active=true]:shadow-none mb-4 active:translate-y-0.5"
+        >
+          {isRunning? 'Pause' : 'Start'}
+        </button>
+      </div>
+      
     </>
   )
 }
@@ -40,14 +55,21 @@ function TimerDisplay({ timer }: { timer: number }) {
   const display = ('0' + Math.floor(timer / 60)).slice(-2) + ':' + ('0' + (timer % 60)).slice(-2);
 
   return (
-    <div className='flex items-center text-8xl font-bold bg-amber-600 text-center size-fit p-8'>
+    <div className='text-5xl sm:text-8xl text-center font-bold bg-amber-600 size-full px-2 py-4 xs:py-8'>
       { display }
     </div>
   )
 }
 
+function notify(stage: stage_t) {
+  if(Notification.permission === 'granted') {
+    new Notification(stage + ' timeout')
+    console.log('notified')
+  }
+}
+
 const timeSetting: {[key: string]: number} = {
   Pomodoro: 1500,
-  'Short Break': 300,
+  'Short Break': 3,
   'Long Break': 600,
 }
